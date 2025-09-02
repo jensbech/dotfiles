d31833fa0cd2346e7831e56ad9ffbe779ad02252
Jens Bech-Sørensen <jens@bechsor.no>
2025-09-02 20:30:32 +0200

    yep

---
+++ setup/brew.sh
@@ -0,0 +1,28 @@
+# cli
+cli_apps=(
+  gh
+  lazygit
+  lazydocker
+  stow
+  helix
+  eza
+  yazi
+)
+
+brew install "${cli_apps[@]}"
+
+# gui
+gui_apps=(
+  bitwarden
+  protonvpn
+  alacritty
+  visual-studio-code
+  spotify
+  raycast
+  discord
+  nikitabobko/tap/aerospace
+  signal
+)
+
+brew install --cask "${gui_apps[@]}"
+
---
+++ setup/scrips.sh
@@ -0,0 +1,6 @@
+# oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# zsh autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
